bf6305b60cb6198fc323193528e1a03cf70f8541
Brian Hare <bhare1985@gmail.com>
2016-03-26 20:52:20 -0400

    Dependency, Versions, and Nuget Spec

--- ArcGISRuntimeWKT/Properties/AssemblyInfo.cs
+++ ArcGISRuntimeWKT/Properties/AssemblyInfo.cs
@@ -36,6 +36,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.*")]
+[assembly: AssemblyFileVersion("0.0.1")]
 [assembly: InternalsVisibleTo("ArcGISRuntimeWKTTests")]
